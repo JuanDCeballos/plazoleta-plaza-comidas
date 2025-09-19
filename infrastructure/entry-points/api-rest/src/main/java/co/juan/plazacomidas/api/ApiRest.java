3aeb675f9b407436708842c2bf04ac61fad82de5
Juan Ceballos <juandavi789@hotmail.com>
2025-09-18 22:56:54 -0500

    Se adiciona los archivos necesarios para que el api-rest funcione, para poder exponer los endpoinst de este microservicio

---
+++ infrastructure/entry-points/api-rest/src/main/java/co/juan/plazacomidas/api/ApiRest.java
@@ -0,0 +1,39 @@
+package co.juan.plazacomidas.api;
+
+import co.juan.plazacomidas.api.dto.ApiResponse;
+import co.juan.plazacomidas.api.dto.restaurante.RestauranteRequestDto;
+import co.juan.plazacomidas.api.dto.restaurante.RestauranteResponseDto;
+import co.juan.plazacomidas.api.utils.RestauranteMapper;
+import co.juan.plazacomidas.model.restaurante.Restaurante;
+import co.juan.plazacomidas.usecase.restaurante.RestauranteUseCase;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/restaurantes", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+public class ApiRest {
+
+    private final RestauranteUseCase restauranteUseCase;
+    private final RestauranteMapper restauranteMapper;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<RestauranteResponseDto>> crearRestaurante(@Valid @RequestBody RestauranteRequestDto requestDto) {
+        Restaurante restaurante = restauranteMapper.toRestaurante(requestDto);
+
+        Restaurante restauranteGuardado = restauranteUseCase.crearRestaurante(restaurante);
+
+        RestauranteResponseDto responseDto = restauranteMapper.toRestauranteResponseDto(restauranteGuardado);
+
+        ApiResponse<RestauranteResponseDto> apiResponse = new ApiResponse<>(responseDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+}
